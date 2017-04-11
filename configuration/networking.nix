a9b4e0bddd08f9c0147fdc37f61d178bced42d42
John Ramsden <johnramsden@users.noreply.github.com>
2017-04-11 16:45:28 -0700

    Setup static ip

--- configuration/networking.nix
+++ configuration/networking.nix
@@ -6,8 +6,16 @@
     hostId = "14ac0214";
 
     defaultGateway = {
-      address = "172.20.20.2;
+      address = "172.20.20.1";
       interface = "eno1"
+    };
+    
+    domain = ramsden.network;
+
+    interfaces = {
+      eno1 = {
+        ip4 = [ { address = "172.20.20.2"; prefixLength = 24; } ];
+      };
     };
 
   };
