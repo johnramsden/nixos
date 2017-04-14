8ace549b8c75dadb8f79db753cb0aa31b1828ab0
John Ramsden <johnramsden@users.noreply.github.com>
2017-04-13 23:55:59 -0700

    Removed automount

--- configuration/networking.nix
+++ configuration/networking.nix
@@ -19,26 +19,6 @@
 
   };
 
-  ## Systemd Network Mounts ##
-  systemd.mounts = [
-    { documentation = [ "https://www.freedesktop.org/software/systemd/man/systemd.mount.html" ];
-      description = "NFS mount unit for Series/Series on lilan";
-      what = "lilan.ramsden.network:/mnt/tank/media/Series/Series";
-      where = "/mnt/lilan/media/Series/Series";
-      type = "nfs";
-      mountConfig = {
-        TimeoutSec = "175";
-        Options = "timeo=15,user,noauto";
-      };
-    }
-  ];
-
-  systemd.automounts = [
-    { documentation = [ "https://www.freedesktop.org/software/systemd/man/systemd.automount.html" ];
-      description = "NFS automount for Series/Series on lilan";
-      where = "/mnt/lilan/media/Series/Series";
-      automountConfig = { TimeoutIdleSec = "175"; };
-    }
-  ];
+  ## Network Mounts ##
 
 }
