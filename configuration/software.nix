a91a0dde5e187b920d96c520a2a860b77d8baf3a
John Ramsden <johnramsden@users.noreply.github.com>
2017-04-15 00:38:36 -0700

    Added zsh config

--- configuration/software.nix
+++ configuration/software.nix
@@ -5,51 +5,52 @@
   nixpkgs.config.allowUnfree = true;
 
   # Packages installed in system profile.
-  environment.systemPackages = with pkgs; 
+  environment.systemPackages = with pkgs;
     # System Administration
     [ wget curl git ] ++
     # Networking
-    [ nfs-utils ] ++
+    [ nfs-utils libnfsidmap ] ++
+    # Nix Specific
+    [ nix-repl ] ++
     ## USER - 'john' ##
     # Shell and related
     [ oh-my-zsh ] ++
     # Userspace utilities
     [  pavucontrol ] ++
     # General user applications
-    [ atom yakuake google-chrome xvkbd ] ++
-    # Programming 
+    [ atom yakuake google-chrome xvkbd jitsi hexchat ] ++
+    # Programming
     [ gitkraken idea.clion ] ++
-    [
-    kdeApplications.akonadi-contacts
-    kdeApplications.akonadi-mime
-    kdeApplications.baloo-widgets
-    kdeApplications.dolphin-plugins
-    kdeApplications.kcachegrind
-    kdeApplications.kdegraphics-mobipocket
-    kdeApplications.kdegraphics-thumbnailers
-    kdeApplications.kdelibs
-    kdeApplications.kdenetwork-filesharing
-    kdeApplications.kdenlive
-    kdeApplications.kdf
-    kdeApplications.kgpg
-    kdeApplications.khelpcenter
-    kdeApplications.kig
-    kdeApplications.kio-extras
-    kdeApplications.kmime
-    kdeApplications.kmix
-    kdeApplications.kompare
-    kdeApplications.konsole
-    kdeApplications.kwalletmanager
-    kdeApplications.libkdcraw
-    kdeApplications.libkexiv2
-    kdeApplications.libkipi
-    kdeApplications.libkomparediff2
-    kdeApplications.marble
-    kdeApplications.okteta
-    kdeApplications.okular
-    kdeApplications.print-manager
-    kdeApplications.spectacle    
-  ];
+    ## KDE ##
+    [ kdeApplications.akonadi-contacts
+      kdeApplications.akonadi-mime
+      kdeApplications.baloo-widgets
+      kdeApplications.dolphin-plugins
+      kdeApplications.kcachegrind
+      kdeApplications.kdegraphics-mobipocket
+      kdeApplications.kdegraphics-thumbnailers
+      kdeApplications.kdelibs
+      kdeApplications.kdenetwork-filesharing
+      kdeApplications.kdenlive
+      kdeApplications.kdf
+      kdeApplications.kgpg
+      kdeApplications.khelpcenter
+      kdeApplications.kig
+      kdeApplications.kio-extras
+      kdeApplications.kmime
+      kdeApplications.kmix
+      kdeApplications.kompare
+      kdeApplications.konsole
+      kdeApplications.kwalletmanager
+      kdeApplications.libkdcraw
+      kdeApplications.libkexiv2
+      kdeApplications.libkipi
+      kdeApplications.libkomparediff2
+      kdeApplications.marble
+      kdeApplications.okteta
+      kdeApplications.okular
+      kdeApplications.print-manager
+      kdeApplications.spectacle  ];
 
   programs.zsh.enable = true;
 
