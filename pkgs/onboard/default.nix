e539a4196097874baa85a5e8bb8fbd2d80d65fa6
John Ramsden <johnramsden@users.noreply.github.com>
2017-04-16 16:11:42 -0700

    Added patches to onboard. Now compiles halfway.

--- pkgs/onboard/default.nix
+++ pkgs/onboard/default.nix
@@ -8,23 +8,34 @@ stdenv.mkDerivation rec {
     sha256 = "01cae1ac5b1ef1ab985bd2d2d79ded6fc99ee04b1535cc1bb191e43a231a3865";
   };
 
-  buildInputs = [ "gtk3"
-                 "python3"
-                 "hunspell"
-                 "isocodes"
-                 "libcanberra_gtk3"
-                 "xorg.libxkbfile"
-                 "python35Packages.pycairo"
-                 "python35Packages.dbus-python"
-                 "python35Packages.pygobject3"
-                 "python35Packages.systemd"
-                 "libudev"
-                 "python35Packages.distutils_extra" ];
+  buildInputs = [ gtk3
+                 python3
+                 hunspell
+                 isocodes
+                 libcanberra_gtk3
+                 xorg.libxkbfile
+                 libxkbcommon
+                 python35Packages.pycairo
+                 python35Packages.dbus-python
+                 python35Packages.pygobject3
+                 python35Packages.systemd
+                 libudev
+                 python35Packages.distutils_extra
+                 gnome3.dconf
+                 pkgconfig
+                 xorg.libXtst ];
 
   buildPhase = ''
+    sed -i 's:/bin/bash:${bash}/bin/bash:' ./setup.py
+    mv ./Onboard/pypredict/tools/split_corpus ./Onboard/pypredict/tools/split_corpus.py
+    mv ./Onboard/pypredict/tools/letter_frequencies ./Onboard/pypredict/tools/letter_frequencies.py
+    mv ./Onboard/pypredict/tools/model_info ./Onboard/pypredict/tools/model_info.py
+    sed -i 's:/bin/bash:${bash}/bin/bash:' ./Onboard/pypredict/tools/split_corpus.py
     python3 "./setup.py" build
+    echo "Finished build"
   '';
   installPhase = ''
+  echo "Starting install phase"
     python3 "./setup.py" install --prefix=$out
   '';
 
