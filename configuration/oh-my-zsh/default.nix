77f379cfceca625aa31aa22d332a4d482b5c1c8e
John Ramsden <johnramsden@users.noreply.github.com>
2017-04-15 23:04:37 -0700

    Shortened zshrc

--- configuration/oh-my-zsh/default.nix
+++ configuration/oh-my-zsh/default.nix
@@ -4,32 +4,20 @@
   ##  ZSH Configuration  ##
   programs.zsh.interactiveShellInit = ''
     COMPUTER="atom";
-    # Path to your oh-my-zsh installation.
-    ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh/
-    # History file
+    ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh/                    # OMZ install
     HISTFILE=$HOME/Computer/Profiles/oh-my-zsh/zsh_history
-    # Change custom directory
-    ZSH_CUSTOM=$HOME/Computer/Profiles/oh-my-zsh/custom
-    # Theme
+    ZSH_CUSTOM=$HOME/Computer/Profiles/oh-my-zsh/custom       # OMZ directory
     ZSH_THEME="af-magic"
-    # Dont auto update
     DISABLE_AUTO_UPDATE=true
-    # Load plugins
+
     plugins=(git systemd ruby gem history sudo node npm nvm zsh-autosuggestions )
 
     source $ZSH/oh-my-zsh.sh
 
-    ## -------- XDG -------- ##
-
-    # XDG config location
-    export XDG_CONFIG_HOME=$HOME/.config
-    # Set XDG dirs with xdg-user-dirs-update, e.g:
-    # xdg-user-dirs-update --set DOCUMENTS  $HOME/University/Documents
-
     ## -------- MISC -------- ##
 
+    export XDG_CONFIG_HOME=$HOME/.config
     export ELECTRON_TRASH=kioclient5
-    # Default editor
     export EDITOR="nano"
   '';
 
