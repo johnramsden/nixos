377b800289bb50a7fdd17661d7bda071fbf6fbf9
John Ramsden <johnramsden@users.noreply.github.com>
2017-04-20 14:30:10 -0700

    Removed onboard From current build.

--- configuration/software.nix
+++ configuration/software.nix
@@ -5,56 +5,12 @@
   ## Packages ##
   nixpkgs.config.allowUnfree = true;
 
-  # Required for thunderbird branding
-  nixpkgs.config.permittedInsecurePackages = [ "libplist-1.12" ];
+  nixpkgs.config.permittedInsecurePackages = [
+         "libplist-1.12"
+       ];
 
   # Packages installed in system profile.
   environment.systemPackages = with pkgs;
-  let c-onboard = with pkgs; python35Packages.buildPythonApplication rec {
-    name = "onboard-${version}";
-    majorVersion = "1.4";
-    version = "${majorVersion}.1";
-    src = fetchurl {
-      url = "https://launchpad.net/onboard/${majorVersion}/${version}/+download/${name}.tar.gz";
-      sha256 = "01cae1ac5b1ef1ab985bd2d2d79ded6fc99ee04b1535cc1bb191e43a231a3865";
-    };
-
-    doCheck = false;
-
-  propagatedBuildInputs = [ gtk3
-                   python3
-                   hunspell
-                   isocodes
-                   libcanberra_gtk3
-                   xorg.libxkbfile
-                   libxkbcommon
-                   python35Packages.pycairo
-                   python35Packages.dbus-python
-                   python35Packages.pygobject3
-                   python35Packages.systemd
-                   libudev
-                   python35Packages.distutils_extra
-                   gnome3.dconf
-                   pkgconfig
-                   xorg.libXtst
-                   bash ];
-
-    preBuild = ''
-      sed -i 's:/bin/bash:${bash}/bin/bash:' ./setup.py
-      for file in Onboard/pypredict/attic/*; do
-        echo "Copying $file to $file.py"
-        cp $file $file.py;
-      done
-    '';
-
-    meta = {
-      homepage = https://launchpad.net/onboard;
-      description = "An onscreen keyboard useful for tablet PC users and for mobility impaired users.";
-      license = stdenv.lib.licenses.gpl3;
-    };
-  };
-
-  in [ c-onboard ] ++
     # System Administration
     [ wget curl git unzip yakuake ] ++
     # Networking
@@ -144,5 +100,48 @@
 
   programs.zsh.enable = true;
 
+  /*let c-onboard = with pkgs; python35Packages.buildPythonApplication rec {
+    name = "onboard-${version}";
+    majorVersion = "1.4";
+    version = "${majorVersion}.1";
+    src = fetchurl {
+      url = "https://launchpad.net/onboard/${majorVersion}/${version}/+download/${name}.tar.gz";
+      sha256 = "01cae1ac5b1ef1ab985bd2d2d79ded6fc99ee04b1535cc1bb191e43a231a3865";
+    };
 
+    doCheck = false;
+
+  propagatedBuildInputs = [ gtk3
+                   python3
+                   hunspell
+                   isocodes
+                   libcanberra_gtk3
+                   xorg.libxkbfile
+                   libxkbcommon
+                   python35Packages.pycairo
+                   python35Packages.dbus-python
+                   python35Packages.pygobject3
+                   python35Packages.systemd
+                   libudev
+                   python35Packages.distutils_extra
+                   gnome3.dconf
+                   pkgconfig
+                   xorg.libXtst
+                   bash ];
+
+    preBuild = ''
+      sed -i 's:/bin/bash:${bash}/bin/bash:' ./setup.py
+      for file in Onboard/pypredict/attic/*; do
+        echo "Copying $file to $file.py"
+        cp $file $file.py;
+      done
+    '';
+
+    meta = {
+      homepage = https://launchpad.net/onboard;
+      description = "An onscreen keyboard useful for tablet PC users and for mobility impaired users.";
+      license = stdenv.lib.licenses.gpl3;
+    };
+  };
+  in [ c-onboard ];*/
 }
