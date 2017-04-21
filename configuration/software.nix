ff605d6276b97d9155dccad9dd4cc52b70ed5a14
John Ramsden <johnramsden@users.noreply.github.com>
2017-04-20 23:28:27 -0700

    Packaged nylas-mail

--- configuration/software.nix
+++ configuration/software.nix
@@ -11,8 +11,55 @@
 
   # Packages installed in system profile.
   environment.systemPackages = with pkgs;
+  let
+    packaged-nylas-mail = with pkgs; stdenv.mkDerivation rec {
+      name = "${pkgname}-${version}";
+      pkgname = "packaged-nylas-mail";
+      version = "2.0.16";
+      subVersion = "4d74cdf";
+
+      src = fetchurl {
+        url = "https://edgehill.s3-us-west-2.amazonaws.com/${version}-${subVersion}/linux-deb/x64/NylasMail.deb";
+        sha256 = "eeaba98898952a8ba3c09ed1fd786dec9e8e977a43b90254e06b13fc24b77cce";
+      };
+
+      phases = [ "unpackPhase" ];
+
+      unpackPhase = ''
+        source $stdenv/setup # Not sure if neccesary
+        PATH=$dpkg/bin:$PATH
+
+        ${dpkg}/bin/dpkg-deb -x $src unpacked
+        cp -r unpacked/* $out/
+      '';
+      meta = {
+        description = "Nylas Mail is an open-source mail client built on the modern web with Electron, React, and Flux. It is designed to be extensible, so it's easy to create new experiences and workflows around email.";
+        license = stdenv.lib.licenses.gpl3;
+        homepage = https://nylas.com;
+      };
+    };
+    nylas-mail = with pkgs; buildFHSUserEnv {
+      name = "nylas-mail";
+      targetPkgs = pkgs: [
+        packaged-nylas-mail
+        gnome2.gtk
+        libgnome_keyring
+        gnome2.gnome_keyring
+        desktop_file_utils
+        python2
+        gnome2.GConf
+        nodejs
+        libnotify
+        xorg.libXtst
+        alsaLib
+        xorg.libXScrnSaver
+      ];
+      multiPkgs = pkgs: [ pkgs.dpkg ];
+      runScript = "./usr/share/nylas-mail/nylas";
+    };
+  in [ nylas-mail ] ++
     # System Administration
-    [ oh-my-zsh wget curl git unzip yakuake nix-repl ] ++
+    [ oh-my-zsh wget curl git unzip yakuake nix-repl binutils dpkg ] ++
     # Networking
     [ nfs-utils libnfsidmap ] ++
     # utilities
@@ -58,6 +105,8 @@
 
 
   programs.zsh.enable = true;
+
+
 
   /*let packaged-onboard = with pkgs; python35Packages.buildPythonApplication rec {
     name = "onboard-${version}";
