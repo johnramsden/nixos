e9f4b6aa545543ebbfa6e93e6addd2ce219c7981
John Ramsden <johnramsden@users.noreply.github.com>
2017-04-22 00:12:06 -0700

    Put onboard in build.

--- configuration/software.nix
+++ configuration/software.nix
@@ -87,6 +87,47 @@
         spectacle
         ];
 
+        packaged-onboard = with pkgs; python35Packages.buildPythonApplication rec {
+          name = "onboard-${version}";
+          majorVersion = "1.4";
+          version = "${majorVersion}.1";
+          src = fetchurl {
+            url = "https://launchpad.net/onboard/${majorVersion}/${version}/+download/${name}.tar.gz";
+            sha256 = "01cae1ac5b1ef1ab985bd2d2d79ded6fc99ee04b1535cc1bb191e43a231a3865";
+          };
+
+          doCheck = false;
+
+        propagatedBuildInputs = [ gtk3
+                         python3
+                         hunspell
+                         isocodes
+                         libcanberra_gtk3
+                         xorg.libxkbfile
+                         libxkbcommon
+                         python35Packages.pycairo
+                         python35Packages.dbus-python
+                         python35Packages.pygobject3
+                         python35Packages.systemd
+                         libudev
+                         python35Packages.distutils_extra
+                         gnome3.dconf
+                         pkgconfig
+                         xorg.libXtst
+                         bash ];
+
+          preBuild = ''
+            rm -r Onboard/pypredict/attic
+            sed -i 's:/bin/bash:${bash}/bin/bash:' ./setup.py
+          '';
+
+          meta = {
+            homepage = https://launchpad.net/onboard;
+            description = "An onscreen keyboard useful for tablet PC users and for mobility impaired users.";
+            license = stdenv.lib.licenses.gpl3;
+          };
+        };
+
     # Packages installed in system profile.
     in systemAdministration ++
       networking ++
@@ -95,7 +136,8 @@
       multimedia ++
       internet ++
       programming ++
-      kdeSoftware;
+      kdeSoftware ++
+      [ packaged-onboard ];
 
   programs.zsh.enable = true;
 
