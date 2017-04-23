bcc854cf2c954c1728240ab7c203f6393240de18
John Ramsden <johnramsden@users.noreply.github.com>
2017-04-23 01:07:38 -0700

    Moved Profiles location to keep computers namespaced..

--- users/shells/oh-my-zsh/default.nix
+++ users/shells/oh-my-zsh/default.nix
@@ -3,10 +3,10 @@
 {
   ##  ZSH Configuration  ##
   environment.interactiveShellInit = ''
-    COMPUTER="atom";
-    ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh/                    # OMZ install
-    HISTFILE=$HOME/Computer/Profiles/oh-my-zsh/zsh_history
-    ZSH_CUSTOM=$HOME/Computer/Profiles/oh-my-zsh/custom       # OMZ directory
+    COMPUTER="atom"
+    ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh/
+    HISTFILE=$HOME/Computer/System/atom/oh-my-zsh/zsh_history
+    ZSH_CUSTOM=$HOME/Computer/System/atom/oh-my-zsh/custom
     ZSH_THEME="af-magic"
     DISABLE_AUTO_UPDATE=true
 
