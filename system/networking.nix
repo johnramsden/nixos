26b7c801fa32a4f5cc6f79e554398bf5a50da0ce
John Ramsden <johnramsden@users.noreply.github.com>
2017-04-30 23:10:08 -0700

    Configured mail daemon with ssmtp, added 'secrets' submodule to hold passwords.

--- system/networking.nix
+++ system/networking.nix
@@ -9,16 +9,20 @@
     interfaces.eno1.ip4 = [ { address = "172.20.20.2"; prefixLength = 24; } ];
     nameservers = [ "172.20.20.1" "8.8.8.8" ];
     domain = "ramsden.network";
+
+    # SSMTP Configuration
+    defaultMailServer = {
+      directDelivery = true;
+      root = "system@atom.ramsden.network";
+      domain = "fastmail.com";
+      authUser = "ramsdenj@fastmail.com";
+      authPass = builtins.readFile /etc/nixos/secrets/ssmtp/password;
+      hostName = "mail.messagingengine.com:465";
+      useTLS = true;
+      useSTARTTLS = false;
+      setSendmail = true;
+    };
   };
 
   time.timeZone = "Canada/Pacific";
-
-  ## Network Services ##
-  services = {
-    rpcbind.enable = true;
-    znapzend.enable = true;
-  };
-
-  ## Network Mounts ##
-
 }
