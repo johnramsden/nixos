78e8a978f5672ceb5db4aebd56d9c7449799e6b9
John Ramsden <johnramsden@users.noreply.github.com>
2017-05-20 17:11:53 -0700

    created qemu user config with shellinit

--- users/john/environment.nix
+++ users/john/environment.nix
@@ -2,16 +2,22 @@
 
 let
   user = "john";
-  configurations = [
-        { name = "libvirt";
-          path = "/home/${user}/.config/libvirt/qemu.conf";
-          uconfig = ''
-            nvram = [ "${pkgs.OVMF}/FV/OVMF_CODE.fd:${pkgs.OVMF}/FV/OVMF_VARS.fd" ]
-          '';
-        }
-  ];
+  configurations = {
+        libvirt = {
+          paths = {
+            file = "/home/${user}/.config/libvirt/qemu.conf";
+            dir = "/home/${user}/.config/libvirt/";
+          };
+          user_config = ''nvram = [ "${pkgs.OVMF}/FV/OVMF_CODE.fd:${pkgs.OVMF}/FV/OVMF_VARS.fd" ]'';
+        };
+  };
 
 in
 {
-  environment_user = map ({ name, path, uconfig }: pkgs.writeTextFile path uconfig) configurations;
+  environment.extraInit = ''
+    if [ $USER==${user} ]; then
+      mkdir -p "${configurations.libvirt.paths.dir}" && echo '${configurations.libvirt.user_config}' > "${configurations.libvirt.paths.file}"
+    fi
+  '';
+
 }
