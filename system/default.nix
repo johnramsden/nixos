43a2e41373f17771821385f76ced3511eccd37a2
John Ramsden <johnramsden@users.noreply.github.com>
2017-05-26 02:06:44 -0700

    Add wine requirements.

--- system/default.nix
+++ system/default.nix
@@ -16,7 +16,7 @@
       enable = true;
       # 32bit ALSA apps integration, needed for steam
       support32Bit = true;
-  };
+    };
     # Acceleration, audio for 32-bit programs, needed for steam
     opengl.driSupport32Bit = true;
   };
